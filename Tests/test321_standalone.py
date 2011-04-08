48cc9b4c9195901422a9b67f2750c792b881f3db
rpmuller <rpmuller@64417113-1622-0410-aef8-ef15d1a3721e>
2011-04-08 22:03:33 +0000

    minor changes to the 321 test
    git-svn-id: https://svn.code.sf.net/p/pyquante/code/trunk@185 64417113-1622-0410-aef8-ef15d1a3721e

--- Tests/test321_standalone.py
+++ Tests/test321_standalone.py
@@ -4,6 +4,7 @@ from PyQuante.PyQuante2 import SCF
 hf = SCF(h2,method="HF",basis="3-21g")
 print h2
 print hf.basis_set.bfs.bfs
+print hf.DoAveraging
 hf.iterate()
 print hf.iterator
 print hf.energy, "(benchmark = -1.122956)"

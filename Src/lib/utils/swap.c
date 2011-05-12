64094ca5b57d8254d5aeec6c1a921fc6465c8edc
baoilleach <baoilleach@64417113-1622-0410-aef8-ef15d1a3721e>
2011-05-12 08:55:36 +0000

    Use malloc/free as compile failed under MSVC. Ask rpmuller to check if okay.
    git-svn-id: https://svn.code.sf.net/p/pyquante/code/trunk@188 64417113-1622-0410-aef8-ef15d1a3721e

--- Src/lib/utils/swap.c
+++ Src/lib/utils/swap.c
@@ -1,11 +1,13 @@
 #include "swap.h"
+#include <stdlib.h>
 #include <string.h>
 
 /* Swap generic function */
 void swap(void *vp1, void *vp2, int size)
 {
-  char buffer[size];
+  char* buffer = (char*) malloc(size*sizeof(char));
   memcpy(buffer, vp1, size);
   memcpy(vp1,vp2,size);
   memcpy(vp2,buffer,size);
+  free(buffer);
 }
